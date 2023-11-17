238207cd24e21fbe0d0e9fb395785121b0e7e087
Dan <danx8076@stud.kea.dk>
2023-11-17 14:28:43 +0100

    first commit

---
+++ src/main/java/memgrp/memorize/MemorizeApplication.java
@@ -0,0 +1,13 @@
+package memgrp.memorize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemorizeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemorizeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/memgrp/memorize/MemorizeApplicationTests.java
@@ -0,0 +1,13 @@
+package memgrp.memorize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemorizeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
