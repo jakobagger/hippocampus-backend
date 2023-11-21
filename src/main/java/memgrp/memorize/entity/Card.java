cd368f23dafaeea3e988d4ee7b179b25aa22c705
hannan <hannanxnoor@gmail.com>
2023-11-21 12:36:44 +0100

    Create entity and orm for card

---
+++ src/main/java/memgrp/memorize/entity/Card.java
@@ -0,0 +1,43 @@
+package memgrp.memorize.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int cardId;
+    private String value;
+    private String suit;
+    private String person;
+    private String action;
+    private String object;
+
+
+    public Card(int cardId, String value, String suit, String person, String action, String object) {
+        this.cardId = cardId;
+        this.value = value;
+        this.suit = suit;
+        this.person = person;
+        this.action = action;
+        this.object = object;
+    }
+
+
+
+
+
+
+
+
+
+}
