70d24e29f5601f615cd0ba84c7b22a7b7057beca
Selmerr <molybdenum93@gmail.com>
2023-11-24 10:06:50 +0100

    get method for card

--- src/main/java/memgrp/memorize/service/CardService.java
+++ src/main/java/memgrp/memorize/service/CardService.java
@@ -1,4 +1,23 @@
 package memgrp.memorize.service;
 
+import memgrp.memorize.dto.CardResponse;
+import memgrp.memorize.entity.Card;
+import memgrp.memorize.repository.CardRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CardService {
+
+    CardRepository cardRepository;
+
+    public CardService(CardRepository cardRepository) {
+        this.cardRepository = cardRepository;
+    }
+    public List<CardResponse> getCards() {
+        List<Card> cards = cardRepository.findAll();
+        List<CardResponse> response = cards.stream().map((card -> new CardResponse(card))).toList();
+        return response;
+    }
 }
