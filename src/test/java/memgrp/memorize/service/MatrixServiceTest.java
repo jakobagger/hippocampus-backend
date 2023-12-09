459512c529ad2e8848ae9950a0ce457ebff21e78
Dan Jørgensen <danx8076@stud.kea.dk>
2023-12-09 18:01:20 +0100

    Tests: addMatrix,addCardToMatrix

--- src/test/java/memgrp/memorize/service/MatrixServiceTest.java
+++ src/test/java/memgrp/memorize/service/MatrixServiceTest.java
@@ -1,6 +1,9 @@
 package memgrp.memorize.service;
 
+import jakarta.persistence.EntityNotFoundException;
+import memgrp.memorize.dto.MatrixRequest;
 import memgrp.memorize.dto.MatrixResponse;
+import memgrp.memorize.entity.Card;
 import memgrp.memorize.entity.Matrix;
 import memgrp.memorize.entity.Suit;
 import memgrp.memorize.entity.Value;
@@ -11,12 +14,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -51,7 +56,6 @@ class MatrixServiceTest {
         assertEquals("Default", matrixList.get(0).getMatrixName());
         verify(matrixRepository).findAll();
     }
-
     @Test
     void getDefaultMatrix() {
         when(matrixRepository.findById(1)).thenReturn(Optional.of(m1));
@@ -59,5 +63,68 @@ class MatrixServiceTest {
         assertEquals(1, response.getMatrixId());
         verify(matrixRepository).findById(1);
     }
+    @Test
+    void testAddMatrix_Success() {
+        int matrixId = 1;
+        String matrixName = "Mock Matrix";
+        MatrixRequest mockRequest = new MatrixRequest(matrixId, matrixName);
 
+        Matrix mockMatrix = new Matrix(matrixId, matrixName);
+        when(matrixRepository.existsById(matrixId)).thenReturn(false);
+        when(matrixRepository.save(any(Matrix.class))).thenReturn(mockMatrix);
+
+        MatrixResponse response = matrixService.addMatrix(mockRequest);
+
+        assertNotNull(response);
+        assertEquals(matrixId, response.getMatrixId());
+    }
+    @Test
+    void testAddMatrix_MatrixExists() {
+        int matrixId = 1;
+        String matrixName = "Mock Matrix";
+        MatrixRequest mockRequest = new MatrixRequest(matrixId, matrixName);
+
+        when(matrixRepository.existsById(matrixId)).thenReturn(true);
+
+        assertThrows(ResponseStatusException.class, () -> {
+            matrixService.addMatrix(mockRequest);
+        });
+    }
+    @Test
+    void testAddCardToMatrix_Success() {
+        Value mockValue = new Value("Ace", "Sports Athlete");
+        Suit mockSuit = new Suit("Clubs", "Cool");
+        String person = "Mock Person";
+        String action = "Mock Action";
+        String object = "Mock Object";
+        String base64 = "Mock Base64String";
+
+        Card mockCard = new Card(mockValue, mockSuit, person, action, object, base64);
+        int matrixId = 1;
+        Matrix mockMatrix = new Matrix(matrixId, "Ikke Default");
+        when(matrixRepository.findById(matrixId)).thenReturn(Optional.of(mockMatrix));
+
+        matrixService.addCardToMatrix(mockCard, matrixId);
+
+        verify(matrixRepository).findById(matrixId);
+        verify(matrixRepository).save(mockMatrix);
+        assertTrue(mockMatrix.getCards().contains(mockCard));
+    }
+    @Test
+    void testAddCardToMatrix_MatrixNotFound() {
+        Value mockValue = new Value("Ace", "Sports Athlete");
+        Suit mockSuit = new Suit("Clubs", "Cool");
+        String person = "Mock Person";
+        String action = "Mock Action";
+        String object = "Mock Object";
+        String base64 = "Mock Base64String";
+
+        Card mockCard = new Card(mockValue, mockSuit, person, action, object, base64);
+        int matrixId = 1;
+        when(matrixRepository.findById(matrixId)).thenReturn(Optional.empty());
+
+        assertThrows(EntityNotFoundException.class, () -> {
+            matrixService.addCardToMatrix(mockCard, matrixId);
+        });
+    }
 }
--- src/test/java/memgrp/memorize/service/MemberServiceTest.java
+++ src/test/java/memgrp/memorize/service/MemberServiceTest.java
@@ -12,14 +12,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -75,13 +71,3 @@ public class MemberServiceTest {
         });
     }
 }
-
-
-
-
-
-
-
-
-
-
